6e2360ac68d53bd5dcc3c977ea63bf483fcdc11a
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-10-10 13:47:23 +0300

    add brands

---
+++ src/utils/utils.ts
@@ -0,0 +1,26 @@
+export const KFC_BRANDS = [
+  {
+    name: "FOODCHERI",
+    image: "/img/FC_Black@2x.png"
+  },
+  {
+    name: "SEAZON",
+    image: "/img/logoszn.png"
+  },
+  {
+    name: "MONOPRIX",
+    image: "/img/logomonop.png"
+  },
+  {
+    name: "FRIGONU",
+    image: "/img/logofrigoco.png"
+  },
+  {
+    name: "SEAZON_BE",
+    image: "/img/logosznbe.png"
+  },
+  {
+    name: "EMPLOYEES_MEALS",
+    image: "/img/logorepasperso.png"
+  }
+];
